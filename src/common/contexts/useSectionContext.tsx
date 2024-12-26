96c037341b83b86ea34fe2aad05dc852e947f913
toothlessdev <toothless042@gmail.com>
2024-12-26 20:54:02 +0900

    chore: 사용하지 않는 코드 제거

--- src/common/contexts/useSectionContext.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-
-import { SectionContext } from "./SectionContext";
-
-export const useSectionContext = () => {
-    const context = useContext(SectionContext);
-    if (!context) throw new Error("useSectionContext must be used within a SectionContextProvider");
-    return context;
-};
