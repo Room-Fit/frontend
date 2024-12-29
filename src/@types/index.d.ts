b1126aa73ffda86837f30967db3048dedcf521c1
toothlessdev <toothless042@gmail.com>
2024-12-29 16:27:31 +0900

    feature: SizeProp 타입 정의

---
+++ src/@types/index.d.ts
@@ -0,0 +1,23 @@
+declare type CSSAbsoluteUnit = "cm" | "mm" | "Q" | "in" | "pc" | "pt" | "px";
+declare type CSSRelativeUnit =
+    | "em"
+    | "ex"
+    | "ch"
+    | "rem"
+    | "lh"
+    | "rlh"
+    | "vw"
+    | "vh"
+    | "vmin"
+    | "vmax"
+    | "vb"
+    | "vi"
+    | "svw"
+    | "svh"
+    | "lvw"
+    | "lvh"
+    | "dvw"
+    | "dvh"
+    | "%";
+
+declare type SizeProp = `${number}${CSSAbsoluteUnit | CSSRelativeUnit}`;
