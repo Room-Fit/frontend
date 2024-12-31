3115355a89c489acdacb430b130e800353e4271e
toothlessdev <toothless042@gmail.com>
2024-12-31 18:14:22 +0900

    style: NavBottom position:sticky 속성 변경

--- src/apps/layouts/NavBottom.tsx
+++ src/apps/layouts/NavBottom.tsx
@@ -2,7 +2,7 @@ import { navBottom } from "@/apps/constants/nav";
 
 export const NavBottom = () => {
     return (
-        <nav className="w-full max-w-[500px] h-[70px] fixed bottom-0 z-30 bg-white shadow-2xl shadow-black">
+        <nav className="w-full max-w-[500px] h-[70px] sticky bottom-0 z-30 bg-white shadow-2xl shadow-black">
             <ul className="flex justify-around w-full h-full gap-4 px-2 bg-white ">
                 {navBottom.map((item, index) => {
                     return (
