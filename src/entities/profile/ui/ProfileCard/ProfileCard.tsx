4c39a420fee94ec7727ef830c2ea2c48a09e3d9b
toothlessdev <toothless042@gmail.com>
2025-01-08 03:52:48 +0900

    feature: ProfileCard 클릭시 해당 프로필 상세조회 페이지로 이동

--- src/entities/profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,5 +1,7 @@
 import { ChevronRight } from "lucide-react";
 
+import { useFlow } from "@/apps/stackflow";
+
 import { ProfileCardDecorator } from "@/entities/profile/assets/ProfileCardDecorator";
 import { ProfileCardLogo } from "@/entities/profile/assets/ProfileCardLogo";
 import { cn } from "@/shared/lib";
@@ -15,9 +17,19 @@ export interface ProfileCardProps {
     mbti: string;
 }
 
-export const ProfileCard = ({ className, name, studentId, birthYear, mbti }: ProfileCardProps) => {
+export const ProfileCard = ({
+    className,
+    id,
+    name,
+    studentId,
+    birthYear,
+    mbti,
+}: ProfileCardProps) => {
+    const { push } = useFlow();
+
     return (
         <div
+            onClick={() => push("ProfileDetailPage", { id })}
             className={cn(className, "relative flex p-4 overflow-hidden rounded-lg")}
             style={{
                 background: "linear-gradient(to bottom, #5DA5FF 0%, #0675FF 56%)",
