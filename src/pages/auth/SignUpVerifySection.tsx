469af4e196bae2b69ef107db3044bd6a71509fbc
toothlessdev <toothless042@gmail.com>
2025-01-08 01:24:17 +0900

    style: 회원가입 폰트크기 및 gap 수정

--- src/pages/auth/SignUpVerifySection.tsx
+++ src/pages/auth/SignUpVerifySection.tsx
@@ -15,36 +15,43 @@ export default function SignUpVerifySection() {
                     <ChevronLeft size={24} className="h-8" onClick={() => pop()} />
 
                     <div className="flex flex-col gap-2">
-                        <h1 className="my-2 text-2xl font-bold">회원가입</h1>
+                        <h1 className="my-2 text-xl font-bold">회원가입</h1>
 
-                        <Label htmlFor="email" className="font-semibold">
-                            아이디 (이메일)
-                        </Label>
-                        <div className="flex gap-2">
-                            <Input
-                                id="email"
-                                type="email"
-                                placeholder="아이디(이메일)을 입력해주세요"
-                                className="flex-grow-[6]]"
-                            />
-                            <Button className="flex-grow-[1]">인증하기</Button>
-                        </div>
-
-                        <Label htmlFor="verification-code" className="font-semibold">
-                            인증번호
-                        </Label>
-                        <div className="flex gap-2">
-                            <Input
-                                id="verification-code"
-                                type="verification-code"
-                                placeholder="인증번호"
-                                className="flex-grow-[6]]"
-                            />
-                            <Button className="flex-grow-[1]">확인하기</Button>
+                        <div>
+                            <Label htmlFor="email" className="my-1 text-base font-normal">
+                                아이디 (이메일)
+                            </Label>
+                            <div className="flex gap-2">
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="아이디(이메일)을 입력해주세요"
+                                    className="flex-grow-[6]"
+                                />
+                                <Button className="flex-grow-[1]">인증하기</Button>
+                            </div>
                         </div>
 
                         <div>
-                            <Label htmlFor="password" className="font-semibold">
+                            <Label
+                                htmlFor="verification-code"
+                                className="my-1 text-base font-normal"
+                            >
+                                인증번호
+                            </Label>
+                            <div className="flex gap-2">
+                                <Input
+                                    id="verification-code"
+                                    type="verification-code"
+                                    placeholder="인증번호"
+                                    className="flex-grow-[6]"
+                                />
+                                <Button className="flex-grow-[1]">확인하기</Button>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Label htmlFor="password" className="my-1 text-base font-normal">
                                 비밀번호
                             </Label>
                             <Input
@@ -55,7 +62,7 @@ export default function SignUpVerifySection() {
                         </div>
 
                         <div>
-                            <Label htmlFor="password" className="font-semibold">
+                            <Label htmlFor="password" className="my-1 text-base font-normal">
                                 비밀번호 확인
                             </Label>
                             <Input
@@ -64,12 +71,23 @@ export default function SignUpVerifySection() {
                                 placeholder="비밀번호를 입력해주세요"
                             />
                         </div>
+
+                        <div className="flex gap-2 my-1">
+                            <Button
+                                variant="secondary"
+                                className="w-full border-[1px] border-primary"
+                            >
+                                이전
+                            </Button>
+                            <Button
+                                className="w-full"
+                                onClick={() => push("SignUpPage", { section: 2 })}
+                            >
+                                다음
+                            </Button>
+                        </div>
                     </div>
                 </section>
-
-                <Button className="w-full" onClick={() => push("SignUpPage", { section: 2 })}>
-                    다음
-                </Button>
             </article>
         </Screen>
     );
