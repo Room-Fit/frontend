990a5a653c7e84383155d5d424c31512158eeeee
toothlessdev <toothless042@gmail.com>
2025-01-08 12:30:12 +0900

    feature: ChatInput 에서 forwardRef 로 하위 input 요소로 ref 전달하도록 변경

--- src/entities/chat/ui/ChatInput/ChatInput.tsx
+++ src/entities/chat/ui/ChatInput/ChatInput.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from "react";
+
 import { Send } from "lucide-react";
 
 import { Button, Input } from "@/shared/ui";
@@ -5,11 +7,12 @@ import { Button, Input } from "@/shared/ui";
 interface ChatInputProps extends React.ComponentProps<"input"> {
     children?: React.ReactNode;
 }
-export const ChatInput = ({ ...props }: ChatInputProps) => {
+export const ChatInput = forwardRef<HTMLInputElement, ChatInputProps>(({ ...props }, ref) => {
     return (
         <div className="fixed bottom-0 flex w-full gap-2 px-5 py-2 bg-white h-fit">
             <Input
                 {...props}
+                ref={ref}
                 className="text-sm focus:outline-none focus-visible:outline-none focus-visible:border-0 h-9 rounded-xl bg-dark-100 focus-visible:ring-0 focus:ring-0 focus-visible:ring-offset-0"
             />
             <Button className="p-0 h-9 rounded-xl aspect-square">
@@ -17,4 +20,4 @@ export const ChatInput = ({ ...props }: ChatInputProps) => {
             </Button>
         </div>
     );
-};
+});
