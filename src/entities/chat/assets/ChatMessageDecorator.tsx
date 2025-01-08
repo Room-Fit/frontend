6d6e834a61f3b822ae059e37ac02a8dc83d51798
toothlessdev <toothless042@gmail.com>
2025-01-08 12:17:03 +0900

    asset: ChatMessageDecorator svg 컴포넌트 추가

---
+++ src/entities/chat/assets/ChatMessageDecorator.tsx
@@ -0,0 +1,5 @@
+export const ChatMessageDecorator = (props: React.ComponentProps<"svg">) => (
+    <svg width={17} height={29} viewBox="0 0 17 29" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M16.3218 0.5C6.75744 1.24294 1.80588 4.87096 0.227833 9.52379L8.28277 28.937C3.50843 14.7425 10.8818 4.20215 16.3218 0.5Z" />
+    </svg>
+);
