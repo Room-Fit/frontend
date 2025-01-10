b0b7e9e43ebb7c102b865e0bcbdf30c9305d91f8
toothlessdev <toothless042@gmail.com>
2025-01-11 02:57:12 +0900

    assets: CardDecorator svg 에셋 추가

---
+++ src/shared/assets/CardDecorator.tsx
@@ -0,0 +1,20 @@
+interface CardDecoratorProps extends React.ComponentProps<"svg"> {
+    color?: string;
+}
+export const CardDecorator = ({ color, ...props }: CardDecoratorProps) => (
+    <svg
+        width={187}
+        height={341}
+        viewBox="0 0 187 341"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <g opacity={0.3}>
+            <path
+                d="M179.66 123.935C183.922 126.92 186.526 131.982 186.526 137.433L186.526 323.289C186.526 333.023 179.305 340.94 170.427 340.94H0.554825L0.554825 0.766113L179.66 123.935Z"
+                fill={color}
+            />
+        </g>
+    </svg>
+);
