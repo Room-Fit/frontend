2ed58b8e901780f476289ad628f8f3639a332b6f
toothlessdev <toothless042@gmail.com>
2025-01-11 02:59:07 +0900

    assets: ProfileCardDecorator svg 컴포넌트에 color props 추가

--- src/entities/profile/assets/ProfileCardDecorator.tsx
+++ src/entities/profile/assets/ProfileCardDecorator.tsx
@@ -1,4 +1,7 @@
-export const ProfileCardDecorator = (props: React.ComponentProps<"svg">) => (
+interface ProfileCardDecoratorProps extends React.ComponentProps<"svg"> {
+    color?: string;
+}
+export const ProfileCardDecorator = ({ color, ...props }: ProfileCardDecoratorProps) => (
     <svg
         width={187}
         height={341}
@@ -10,7 +13,7 @@ export const ProfileCardDecorator = (props: React.ComponentProps<"svg">) => (
         <g opacity={0.3}>
             <path
                 d="M179.66 123.935C183.922 126.92 186.526 131.982 186.526 137.433L186.526 323.289C186.526 333.023 179.305 340.94 170.427 340.94H0.554825L0.554825 0.766113L179.66 123.935Z"
-                fill="#5DA5FF"
+                fill={color}
             />
         </g>
     </svg>
