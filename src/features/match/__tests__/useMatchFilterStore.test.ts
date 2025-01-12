99d662f1d2e2021ae5b64fcd13d4b696043b8938
toothlessdev <toothless042@gmail.com>
2025-01-13 03:14:54 +0900

    test: 테스트 이름을 직관적인 네이밍으로 수정

--- src/features/match/__tests__/useMatchFilterStore.test.ts
+++ src/features/match/__tests__/useMatchFilterStore.test.ts
@@ -5,165 +5,163 @@ import {
     MatchFilterState,
 } from "@/features/match/store/useMatchFilterStore";
 
-describe("useMatchFilterStore()", () => {
-    describe("matchFilterReducer()", () => {
-        let state: MatchFilterState;
+describe("룸메이트 모집공고 필터링 상태 변경을 테스트합니다", () => {
+    let state: MatchFilterState;
 
-        beforeEach(() => {
-            state = {
-                numOfAppliedFilters: 0,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            };
+    beforeEach(() => {
+        state = {
+            numOfAppliedFilters: 0,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
+        };
+    });
+
+    test("체크된 모든 '모집상태'를 반영합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.SET_RECRUITMENT_STATUS,
+            payload: ["status1", "status2"],
+        };
+
+        const result = matchFilterReducer(state, action);
+
+        expect(result).toEqual({
+            numOfAppliedFilters: 2,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: ["status1", "status2"],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.SET_RECRUITMENT_STATUS : payload 를 recruitmentStatus 에 반영하고, numOfAppliedFilters 를 업데이트합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.SET_RECRUITMENT_STATUS,
-                payload: ["status1", "status2"],
-            };
+    test("체크된 '모집상태'를 추가합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.ADD_RECRUITMENT_STATUS,
+            payload: "status",
+        };
 
-            const result = matchFilterReducer(state, action);
+        const result = matchFilterReducer(state, action);
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 2,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: ["status1", "status2"],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 1,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: ["status"],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.ADD_RECRUITMENT_STATUS : payload 를 recruitmentStatus 에 추가합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.ADD_RECRUITMENT_STATUS,
-                payload: "status",
-            };
+    test("이미 체크된 '모집상태'를 해제합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.REMOVE_RECRUITMENT_STATUS,
+            payload: "status",
+        };
 
-            const result = matchFilterReducer(state, action);
+        const result = matchFilterReducer(state, action);
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 1,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: ["status"],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 0,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.REMOVE_RECRUITMENT_STATUS : payload 를 recruitmentStatus 에서 제거합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.REMOVE_RECRUITMENT_STATUS,
-                payload: "status",
-            };
+    test("이미 체크된 '모집상태'가 반영되어있는 경우, 해당 체크상태를 해제합니다", () => {
+        state.numOfAppliedFilters = 1;
+        state.recruitmentStatus = ["status"];
 
-            const result = matchFilterReducer(state, action);
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS,
+            payload: "status",
+        };
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 0,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        const result = matchFilterReducer(state, action);
+
+        expect(result).toEqual({
+            numOfAppliedFilters: 0,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS (default: active) : payload 가 존재하는 경우, recruitmentStatus 에서 제거합니다", () => {
-            state.numOfAppliedFilters = 1;
-            state.recruitmentStatus = ["status"];
+    test("이미 체크된 '모집상태'가 반영되어있지 않은 경우, 해당 체크상태를 추가합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS,
+            payload: "status",
+        };
 
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS,
-                payload: "status",
-            };
+        const result = matchFilterReducer(state, action);
 
-            const result = matchFilterReducer(state, action);
-
-            expect(result).toEqual({
-                numOfAppliedFilters: 0,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 1,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: ["status"],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS (default: inactive) : payload 가 존재하지 않는 경우, recruitmentStatus 에 추가합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.TOGGLE_RECRUITMENT_STATUS,
-                payload: "status",
-            };
+    test("최소 모집인원을 설정합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.SET_MIN_RECRUITMENT_PEOPLE,
+            payload: 2,
+        };
 
-            const result = matchFilterReducer(state, action);
+        const result = matchFilterReducer(state, action);
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 1,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: ["status"],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 1,
+            isRecruitmentPeopleSet: true,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 2,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.SET_MIN_RECRUITMENT_PEOPLE : payload 를 minRecruitmentPeople 에 반영하고, isRecruitmentPeopleSet 를 참으로 변경합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.SET_MIN_RECRUITMENT_PEOPLE,
-                payload: 2,
-            };
+    test("최대 모집인원을 설정합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.SET_MAX_RECRUITMENT_PEOPLE,
+            payload: 5,
+        };
 
-            const result = matchFilterReducer(state, action);
+        const result = matchFilterReducer(state, action);
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 1,
-                isRecruitmentPeopleSet: true,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 2,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 1,
+            isRecruitmentPeopleSet: true,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 5,
+            dormitoryType: "전체",
         });
+    });
 
-        test("MatchFilterActionType.SET_MAX_RECRUITMENT_PEOPLE  payload 를 maxRecruitmentPeople 에 반영하고, isRecruitmentPeopleSet 를 참으로 변경합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.SET_MAX_RECRUITMENT_PEOPLE,
-                payload: 5,
-            };
+    test("모든 필터 상태를 초기화합니다", () => {
+        const action: MatchFilterAction = {
+            type: MatchFilterActionType.CLEAR_ALL_FILTERS,
+        };
 
-            const result = matchFilterReducer(state, action);
+        const result = matchFilterReducer(state, action);
 
-            expect(result).toEqual({
-                numOfAppliedFilters: 1,
-                isRecruitmentPeopleSet: true,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 5,
-                dormitoryType: "전체",
-            });
-        });
-
-        test("MatchFilterActionType.CLEAR_ALL_FILTERS : 모든 필터 상태를 초기화합니다", () => {
-            const action: MatchFilterAction = {
-                type: MatchFilterActionType.CLEAR_ALL_FILTERS,
-            };
-
-            const result = matchFilterReducer(state, action);
-
-            expect(result).toEqual({
-                numOfAppliedFilters: 0,
-                isRecruitmentPeopleSet: false,
-                recruitmentStatus: [],
-                minRecruitmentPeople: 1,
-                maxRecruitmentPeople: 6,
-                dormitoryType: "전체",
-            });
+        expect(result).toEqual({
+            numOfAppliedFilters: 0,
+            isRecruitmentPeopleSet: false,
+            recruitmentStatus: [],
+            minRecruitmentPeople: 1,
+            maxRecruitmentPeople: 6,
+            dormitoryType: "전체",
         });
     });
 });
