7b172c80c585506389619267a3355aa075bf765f
kimgho <kgh67811@naver.com>
2025-01-13 00:52:47 +0900

    feature: zustand 추가

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -1,19 +1,14 @@
-import { Provider } from "react-redux";
-
 import { Stack } from "./stackflow";
-import { store } from "./store/store";
 import "./styles/tailwind.css";
 import { queryClient } from "@/shared/lib";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
     return (
-        <Provider store={store}>
-            <QueryClientProvider client={queryClient}>
-                <main className="w-full max-w-[500px] mx-auto h-screen border-[1px]">
-                    <Stack />
-                </main>
-            </QueryClientProvider>
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+            <main className="w-full max-w-[500px] mx-auto h-screen border-[1px]">
+                <Stack />
+            </main>
+        </QueryClientProvider>
     );
 }
