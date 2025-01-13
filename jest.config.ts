b5d983d7aa83c1480c78c2849f0eeb1ed7269639
toothlessdev <toothless042@gmail.com>
2025-01-14 04:53:52 +0900

    fix: jest 환경에서 import.meta.env is not defined 오류 해결

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,12 @@ import { JestConfigWithTsJest } from "ts-jest";
 const config: JestConfigWithTsJest = {
     testEnvironment: "jsdom",
     transform: {
-        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.tsx?$": "babel-jest",
     },
     moduleNameMapper: {
         "^.+\\.svg$": "jest-svg-transformer",
         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "\\.(gif|jpg|jpeg|png|svg|webp)$": "<rootDir>/fileMock.js",
         "@/(.*)$": "<rootDir>/src/$1",
     },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,33 @@
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.gif" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.svg" {
+    const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    export default content;
+}
+
+declare module "*.webp" {
+    const value: string;
+    export default value;
+}
+
 declare type CSSAbsoluteUnit = "cm" | "mm" | "Q" | "in" | "pc" | "pt" | "px";
 declare type CSSRelativeUnit =
     | "em"
