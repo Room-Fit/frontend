5acf531e7fcba221df86f4a4600946f93e2c8a56
toothlessdev <toothless042@gmail.com>
2025-01-14 14:45:06 +0900

    feature: 이메일 코드 전송 및 인증 API 호출함수 작성

--- src/features/auth/service/index.ts
+++ src/features/auth/service/index.ts
@@ -1,1 +1,9 @@
-export const authService = {};
+import { sendEmailVerificationCode } from "@/features/auth/service/emailCode";
+import { verifyEmailVerificationCode } from "@/features/auth/service/emailVerify";
+import { signUp } from "@/features/auth/service/signUp";
+
+export const authService = {
+    signUp,
+    sendEmailVerificationCode,
+    verifyEmailVerificationCode,
+};
