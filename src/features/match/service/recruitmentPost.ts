f05b9f6830a870b86593313e760026b796da9ccb
kimgho <kgh67811@naver.com>
2025-01-16 00:08:58 +0900

    feature: 핏글 글쓰기 모킹API 작성
    - 기숙사 선택 컴포넌트 구현
    - 최대 인원수 선택 컴포넌트 구현
    - MatchListPage에서 key값 추가

---
+++ src/features/match/service/recruitmentPost.ts
@@ -0,0 +1,53 @@
+import ExceptionHandler from "axios-exception-handler";
+
+import { api } from "@/shared/lib";
+
+export type RecruitmentPost = {
+    id: number;
+    title: string;
+    description: string;
+    dormitory:
+        | "봉사관"
+        | "화목관"
+        | "향토관"
+        | "첨성관"
+        | "명의관"
+        | "누리관"
+        | "보람관"
+        | "선의관"
+        | "경애관(상주)"
+        | "근면관(상주)"
+        | "청운관(상주)"
+        | "노악관(상주)";
+    maxQuota: number;
+};
+
+export const MAX_RECRUITEMENT_NUMBER = 6;
+
+export const postRecruitment = async ({
+    id,
+    title,
+    description,
+    dormitory,
+    maxQuota,
+}: RecruitmentPost) => {
+    try {
+        const response = await api.post<RecruitmentPost>("/api/v1/recruiment", {
+            id,
+            title,
+            dormitory,
+            description,
+            maxQuota,
+        });
+        return response.data;
+    } catch (err) {
+        ExceptionHandler(err)
+            .addCase(400, "잘못된 요청입니다.")
+            .addCase(401, "로그인이 필요한 서비스입니다.")
+            .addCase(403, "글쓰기 권한이 없습니다.")
+            .addCase(404, "존재하지 않는 페이지입니다.")
+            .addCase(500, "서버 오류가 발생하였습니다. 관리자에게 문의바랍니다.")
+            .addDefaultCase("알 수 없는 오류가 발생하였습니다.")
+            .handle();
+    }
+};
---
+++ src/features/match/ui/SelectMaxQuota.tsx
@@ -0,0 +1,36 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/shared/ui/select";
+
+interface SelectQuotaProps {
+    value: number;
+    onChange: (value: number) => void;
+}
+
+export const SelectMaxQuota = ({ value, onChange }: SelectQuotaProps) => {
+    const quotaOptions = [2, 3, 4, 5, 6];
+
+    return (
+        <Select value={value.toString()} onValueChange={(val) => onChange(Number(val))}>
+            <SelectTrigger className="w-36">
+                <SelectValue placeholder="인원수 선택" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>최대 인원수</SelectLabel>
+                    {quotaOptions.map((quota) => (
+                        <SelectItem key={quota} value={quota.toString()}>
+                            {quota}명
+                        </SelectItem>
+                    ))}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+};
