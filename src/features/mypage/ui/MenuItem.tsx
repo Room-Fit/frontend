e986483c88221e698b0078b56d45378762711fe2
toothlessdev <toothless042@gmail.com>
2025-01-18 16:32:59 +0900

    feature: MenuItem 에 onClick 프로퍼티 추가

--- src/features/mypage/ui/MenuItem.tsx
+++ src/features/mypage/ui/MenuItem.tsx
@@ -1,11 +1,13 @@
-export interface MenuItemProps {
+import React from "react";
+
+export interface MenuItemProps extends React.ComponentProps<"div"> {
     label: string;
     icon?: React.ReactNode;
 }
 
-export const MenuItem = ({ label, icon }: MenuItemProps) => {
+export const MenuItem = ({ label, icon, ...rest }: MenuItemProps) => {
     return (
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2" {...rest}>
             <span>{icon}</span>
             <span className="font-semibold">{label}</span>
         </div>
