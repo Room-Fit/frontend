d30a8aad1ac46cf78b81162c2a9445e21b5dd459
toothlessdev <toothless042@gmail.com>
2025-01-20 21:19:45 +0900

    fix: 프로필 설문 응답 생성 요청전, 스키마에 맞게 구조 변경

---
+++ src/features/profile/service/createSurveyResponse.ts
@@ -0,0 +1,46 @@
+import ExceptionHandler from "axios-exception-handler";
+
+import { FormSchema, Option } from "@/entities/form/types";
+import { api } from "@/shared/lib";
+import { BaseResponse } from "@/shared/types/BaseResponse";
+
+export type CreateSurveyRequestBody = {
+    questionReplies: {
+        questionId: number;
+        replies: Option[];
+    }[];
+};
+
+export type CreateSurveyResponseBody = {
+    id: number;
+    title: string;
+    description: string;
+    questions: {
+        id: number;
+        title: string;
+        type: string;
+        optionDelimiter: string | null;
+        options: Option[];
+    }[];
+};
+
+export async function createSurveyResponse(formSchema: FormSchema) {
+    const questionReplies = formSchema.questions.map((question) => {
+        return { questionId: question.id, replies: question.options };
+    });
+
+    const body: CreateSurveyRequestBody = { questionReplies };
+
+    try {
+        const { data: response } = await api.post<BaseResponse<CreateSurveyResponseBody>>(
+            "/api/v1/survey/reply",
+            body,
+        );
+        return response.data;
+    } catch (err) {
+        ExceptionHandler(err)
+            .addCase(500, "서버 오류가 발생했습니다.")
+            .addDefaultCase("설문조사 응답 생성에 실패했습니다.")
+            .handle();
+    }
+}
