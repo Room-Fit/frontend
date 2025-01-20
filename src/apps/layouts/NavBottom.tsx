7773f771370f40ed21b6d557241070ccfe08499b
toothlessdev <toothless042@gmail.com>
2025-01-20 17:16:37 +0900

    feature: ProfileModal 컴포넌트 구현

--- src/apps/layouts/NavBottom.tsx
+++ src/apps/layouts/NavBottom.tsx
@@ -1,28 +1,36 @@
+import { Fragment } from "react/jsx-runtime";
+
 import { navBottom } from "@/apps/constants/nav";
 import { ActivityName } from "@/apps/stackflow";
 import { useFlow } from "@/apps/stackflow";
+
+import { ProfileModal } from "@/entities/profile/ui/ProfileModal/ProfileModal";
 
 export const NavBottom = () => {
     const { replace } = useFlow();
 
     return (
-        <nav className="w-full max-w-[500px] h-[70px] sticky bottom-0 z-30 bg-white shadow-2xl shadow-black">
-            <ul className="flex justify-around w-full h-full gap-4 px-2 bg-white ">
-                {navBottom.map((item, index) => {
-                    return (
-                        <li
-                            key={index}
-                            className="flex items-center h-full"
-                            onClick={() => replace(item.activityName as ActivityName, {})}
-                        >
-                            <button className="flex flex-col items-center">
-                                <item.icon size={25} />
-                                <p className="text-sm">{item.label}</p>
-                            </button>
-                        </li>
-                    );
-                })}
-            </ul>
-        </nav>
+        <Fragment>
+            <ProfileModal />
+
+            <nav className="w-full max-w-[500px] h-[70px] sticky bottom-0 z-30 bg-white shadow-2xl shadow-black">
+                <ul className="flex justify-around w-full h-full gap-4 px-2 bg-white ">
+                    {navBottom.map((item, index) => {
+                        return (
+                            <li
+                                key={index}
+                                className="flex items-center h-full"
+                                onClick={() => replace(item.activityName as ActivityName, {})}
+                            >
+                                <button className="flex flex-col items-center">
+                                    <item.icon size={25} />
+                                    <p className="text-sm">{item.label}</p>
+                                </button>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        </Fragment>
     );
 };
