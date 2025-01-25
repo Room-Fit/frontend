fef40d916172524e400508c8e7df8ed667a48d75
toothlessdev <toothless042@gmail.com>
2025-01-25 19:01:47 +0900

    feature: axios interceptor 로 jwt 를 헤더에 추가

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
 
+import { useAuth } from "@/features/auth/store/useAuth";
+
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
     timeout: 10000,
+});
+
+api.interceptors.request.use((config) => {
+    const { accessToken } = useAuth.getState();
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+});
+
+api.interceptors.response.use((response) => {
+    return response;
 });
