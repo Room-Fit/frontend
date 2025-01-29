3cb0a0c74552a8fbef8fa0c8e7c5922028610a34
kimgho <kgh67811@naver.com>
2025-01-30 02:24:11 +0900

    chore: 불필요한 주석 제거

--- src/__mocks__/match/index.ts
+++ src/__mocks__/match/index.ts
@@ -1,9 +1,4 @@
-// import { lookupDetailHandler } from "@/__mocks__/match/lookupDetail";
 import { lookupListHandler } from "@/__mocks__/match/lookupList";
 import { writeRecruitmentHandler } from "@/__mocks__/match/writeRecruiment";
 
-export const matchHandlers = [
-    // ...lookupDetailHandler,
-    ...lookupListHandler,
-    ...writeRecruitmentHandler,
-];
+export const matchHandlers = [...lookupListHandler, ...writeRecruitmentHandler];
