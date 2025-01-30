a4e6e21f93c2b6ff8690741dca038189a124dc3f
kimgho <kgh67811@naver.com>
2025-01-31 00:22:50 +0900

    fix: ProfileCard 선택 시 프로필 조회로 이동
    - 기존 ">"아이콘 말고 카드 전체에 onClick 효과 적용

--- src/entities/profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,20 +1,17 @@
 import { ChevronRight } from "lucide-react";
 
-import { useFlow } from "@/apps/stackflow";
-
 import { Card } from "@/shared/components/Card/Card";
 import { cn } from "@/shared/lib";
 
-export interface ProfileCardProps {
+export interface ProfileCardProps extends React.ComponentProps<"div"> {
     gender?: "M" | "F";
     nickname: string;
     children?: React.ReactNode;
 }
 
-export const ProfileCard = ({ nickname, children, gender }: ProfileCardProps) => {
-    const { push } = useFlow();
+export const ProfileCard = ({ nickname, children, gender, ...rest }: ProfileCardProps) => {
     return (
-        <Card theme={gender === "M" ? "genderMTransparent" : "genderFTransparent"}>
+        <Card theme={gender === "M" ? "genderMTransparent" : "genderFTransparent"} {...rest}>
             <div className="p-4">
                 <div className="flex items-center gap-2">
                     <h1 className="text-xl font-bold">{nickname}</h1>
@@ -24,7 +21,6 @@ export const ProfileCard = ({ nickname, children, gender }: ProfileCardProps) =>
                         "absolute text-primary top-[50%] translate-y-[-50%] right-4",
                         gender === "M" ? "text-primary" : "text-dark-300",
                     )}
-                    onClick={() => push("ProfileDetailPage", { nickname })}
                 />
                 <p className="pt-1 text-sm">{children}</p>
             </div>
--- src/pages/profile/ProfileListPage.tsx
+++ src/pages/profile/ProfileListPage.tsx
@@ -1,23 +1,31 @@
 import { BaseScreen } from "@/apps/Screen";
 import { NavTop } from "@/apps/layouts/NavTop";
+import { useFlow } from "@/apps/stackflow";
 
 import { ProfileCard } from "@/entities/profile/ui/ProfileCard/ProfileCard";
 import { useReadUserList } from "@/features/profile/service/readUserList";
 
 export default function ProfileListPage() {
     const { data } = useReadUserList();
+    const { push } = useFlow();
     const [university] = data ?? [];
 
     const parseStudentId = (studentId: string) => {
         return studentId.toString().substring(2, 4);
     };
-
+    const pushProfileDetailPage = (id: number) => {
+        push("ProfileDetailPage", { id });
+    };
     return (
         <BaseScreen>
             <NavTop />
-            <div className="flex flex-col gap-4 p-4">
+            <div className="flex flex-col gap-4 p-4 ">
                 {data?.map((user) => (
-                    <ProfileCard gender={user.gender} nickname={user.nickname}>
+                    <ProfileCard
+                        gender={user.gender}
+                        nickname={user.nickname}
+                        onClick={() => pushProfileDetailPage(user.id)}
+                    >
                         <div className="flex flex-col font-bold">
                             <p className="text-xs">{parseStudentId(user.studentId)}학번</p>
                             <p className="text-xs">{user.birth}년생</p>
