d1c8a80e3b7b36fa2e58b915fdef276b6d6ac53a
toothlessdev <toothless042@gmail.com>
2025-02-03 16:18:48 +0900

    fix: survey reply 에 BaseResponse 제네릭 타입 적용

--- src/features/profile/service/readSurveyReplyById.ts
+++ src/features/profile/service/readSurveyReplyById.ts
@@ -3,6 +3,7 @@ import ExceptionHandler from "axios-exception-handler";
 import { replyElementMap } from "@/entities/profile/ui/DynamicReply/DynamicReply";
 import { PROFILE_QUERY_KEY_FACTORY } from "@/features/profile/service/keys";
 import { api } from "@/shared/lib";
+import { BaseResponse } from "@/shared/types/BaseResponse";
 import { useQuery } from "@tanstack/react-query";
 
 export type QuestionReply = {
@@ -22,9 +23,10 @@ export type ReadSurveyReplyByIdResponseBody = {
 
 export async function readSurveyReplyById(userId: number) {
     try {
-        const { data: response } = await api.get("/api/v1/survey/reply", {
-            params: { user_id: userId },
-        });
+        const { data: response } = await api.get<BaseResponse<ReadSurveyReplyByIdResponseBody>>(
+            "/api/v1/survey/reply",
+            { params: { user_id: userId } },
+        );
         return response.data;
     } catch (err) {
         ExceptionHandler(err)
