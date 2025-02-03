592a30c19596970a57673550ddf3833bad595e4c
toothlessdev <toothless042@gmail.com>
2025-02-03 15:48:21 +0900

    feature: userId 에 해당하는 survey reply 조회 API 연동

--- src/features/profile/service/index.ts
+++ src/features/profile/service/index.ts
@@ -1,7 +1,9 @@
 import { createSurveyResponse } from "@/features/profile/service/createSurveyResponse";
 import { readRecentSurvey } from "@/features/profile/service/readRecentSurvey";
+import { readSurveyReplyById } from "@/features/profile/service/readSurveyReplyById";
 
 export const profileService = {
+    readSurveyReplyById,
     readRecentSurvey,
     createSurveyResponse,
 };
